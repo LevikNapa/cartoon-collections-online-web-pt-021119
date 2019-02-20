254c6161217a556011e41bae12675a9ff33a6354
Levik Naparstek <levik010@gmail.com>
2019-02-20 21:53:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,6 +31,8 @@ def long_planeteer_calls(array)
 end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-array.find ("cheddar")
+array.find do |kind|
+    cheese_types.include?(type)
+  end 
   
 end
