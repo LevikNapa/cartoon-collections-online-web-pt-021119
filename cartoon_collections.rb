b9591d28ccb5a6d45fe8036163afca2107d7539d
Levik Naparstek <levik010@gmail.com>
2019-02-20 21:08:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves(names)# code an argument here
-  roll_call_dwarves.each_with_index do |name, index|
+def roll_call_dwarves(dwarves)# code an argument here
+  roll_call_dwarves(dwarves).each_with_index do |name, index|
   end
 end
 
